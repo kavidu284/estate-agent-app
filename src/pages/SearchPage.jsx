d2928ae45ab3a0a50fe259c71aa47750eafb7fa5
kavidu <YOUR-GITHUB-NOREPLY-EMAIL>
2025-12-30 13:32:15 +0530

    Search page mistakes fix

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import data from "../data/properties.json";
 
-function SearchPage({ favourites = [], removeFavourite , addFavourite }) {
-
- 
+function SearchPage({
+  favourites = [],
+  removeFavourite = () => {},
+  addFavourite = () => {}
+}) {
 
   const [searchResults, setSearchResults] = useState("Any");
   const [minBedrooms, setMinBedrooms] = useState("");
