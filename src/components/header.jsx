c29cc81ec36c1bb5457d42d1605134342846a291
kavidu <YOUR-GITHUB-NOREPLY-EMAIL>
2025-12-31 11:43:11 +0530

    fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,9 +13,6 @@ function Header() {
           <NavLink to="/" className="nav-item">
             Home
           </NavLink>
-          <NavLink to="/properties" className="nav-item">
-            Properties
-          </NavLink>
           <NavLink to="/SearchPage" className="nav-item">
             Search
           </NavLink>
