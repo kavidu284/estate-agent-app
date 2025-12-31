271c4c3d967ef884c23f1c89c6d2b8e7df0bba2b
kavidu <YOUR-GITHUB-NOREPLY-EMAIL>
2025-12-31 11:28:01 +0530

    property page styling

--- src/pages/PropertyPage.jsx
+++ src/pages/PropertyPage.jsx
@@ -17,73 +17,84 @@ function PropertyPage({addFavourite}) {
   return (
     <div className="container">
       <div className="details">
-      <h2>{property.type}</h2>
-      {/* PROPERTY IMAGE */}
-      <img
-        src={property.picture}
-        alt="Property"
-        className="property-image-2"
-      />
-<div className="thumbnail-grid">
-    {property.Images.map((img, index) => (
-      <img key={index} src={img} alt={`Property ${index + 1}`} />
-    ))}
-  </div>
+        <div className="property-header">
+          <h2>{property.type}</h2>
+          <p className="property-location">{property.location}</p>
+          <p className="property-price">
+            £{property.price.toLocaleString()}
+          </p>
+        </div>
+        {/* PROPERTY IMAGE */}
+        <img
+          src={property.picture}
+          alt="Property"
+          className="property-image-2"
+        />
+        <div className="thumbnail-grid">
+          {property.Images.map((img, index) => (
+            <img key={index} src={img} alt={`Property ${index + 1}`} />
+          ))}
+        </div>
 
-      {/* BASIC INFO */}
-      <p><strong>Location:</strong> {property.location}</p>
-      <p><strong>Bedrooms:</strong> {property.bedrooms}</p>
-      <p><strong>Price:</strong> £{property.price.toLocaleString()}</p>
-      <p><strong>Tenure:</strong> {property.tenure}</p>
-      <p><strong>Added:</strong> {property.added.day} {property.added.month} {property.added.year}</p>
-      <br />
+        {/* BASIC INFO */}
+        <div className="property-info">
+        <div><strong>Bedrooms:</strong> {property.bedrooms}</div>
+        <div><strong>Tenure:</strong> {property.tenure}</div>
+        <div><strong>Added:</strong> {property.added.day} {property.added.month} {property.added.year}</div>
+        </div>
 
-      {/* TABS */}
-      <Tabs>
-  <TabList>
-    <Tab>Description</Tab>
-    <Tab>Floor Plan</Tab>
-    <Tab>Map</Tab>
-  </TabList>
+        {/* TABS */}
+        <Tabs className="property-tabs">
+          <TabList>
+            <Tab>Description</Tab>
+            <Tab>Floor Plan</Tab>
+            <Tab>Map</Tab>
+          </TabList>
 
-  <TabPanel>
-    <p>{property.description}</p>
-  </TabPanel>
+          <TabPanel>
+            <p className="tab-text">{property.description}</p>
+          </TabPanel>
 
-  <TabPanel>
-    <img
-      src={property.floorplan}
-      alt="Floor Plan"
-      style={{ width: "60%" }}
-    />
-  </TabPanel>
+          <TabPanel>
+            <img
+              src={property.floorplan}
+              alt="Floor Plan"
+              className="floorplan-img"
+            />
+          </TabPanel>
 
-  <TabPanel>
-    <p>{property.location}</p>
-    <iframe
-      title="map"
-      width="100%"
-      height="300"
-      loading="lazy"
-      src={`https://www.google.com/maps?q=${property.location}&output=embed`}
-    ></iframe>
-  </TabPanel>
-</Tabs>
+          <TabPanel>
+            <p>{property.location}</p>
+            <iframe
+              title="map"
+              width="100%"
+              height="300"
+              loading="lazy"
+              src={`https://www.google.com/maps?q=${property.location}&output=embed`}
+            ></iframe>
+          </TabPanel>
+        </Tabs>
 
-      <br />
       
-      
-      <button className="add-favourite-btn" onClick={() => addFavourite(property)}>
-         Add to Favourites
-      </button>
+        
+        <div className="property-actions">
+          <button className="add-favourite-btn" onClick={() => addFavourite(property)}>
+            Add to Favourites
+          </button>
+        </div>
+        <div className="property-nav">
+          <Link to="/SearchPage" className="back-link">
+            ← Back to Search
+          </Link>
+          <Link to="/properties" className="back-link">
+            ← Back to All Properties
+          </Link>
+        </div>
 
-
-      <br /><br />
-      <Link to="/SearchPage" className="back-link">← Back to Search</Link>
-      <br />
-      <Link to="/properties" className="back-link">← Back to All Property</Link>
       </div>
-    </div>);
+    </div>
+  );
 }
 
 export default PropertyPage;
+ 
