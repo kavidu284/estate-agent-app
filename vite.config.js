ef9faf56cb9e15283983fa9c584e307e8eea083d
kavidu <YOUR-GITHUB-NOREPLY-EMAIL>
2026-01-01 18:28:11 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,6 @@ export default defineConfig({
     globals: true,                 
     environment: "jsdom",           
     setupFiles: "./src/setupTests.js", 
+    base: '/estate-agent-app/',
 },
 })
